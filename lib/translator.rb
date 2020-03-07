62041521fed61693a6dd1b897010b9621f73a99f
Ras azzab <ras.dinho7@gmail.com>
2020-03-07 17:38:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,23 +1,6 @@
-# # require modules here
-# def load_library(emoticon_file)
-#   emoticons = YAML.load_file('./lib/emoticons.yml')
-
-#   emoticon_lib = {'get_meaning'  => {},
-#                   'get_emoticon' => {} }
-
-#   emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-#     english = value[0]
-#     japanese = value[1]
-#     emoticon_lib['get_meaning'][japanese] = meaning
-#     emoticon_lib['get_emoticon'][english] = japanese
-#   end
-#   emoticon_lib
-# end
 
 require 'yaml'
 require 'pry'
-#emoticon_file = YAML.load_file('./lib/emoticons.yml')
-#emoticon = '(#^.^#)'
 
 def load_library(emoticon_file)
   emoticons = YAML.load_file('./lib/emoticons.yml')
