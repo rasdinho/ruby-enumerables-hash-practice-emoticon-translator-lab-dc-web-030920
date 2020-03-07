98676380caba8ef10b62bb3e04cb5cd39333fe67
Ras azzab <ras.dinho7@gmail.com>
2020-03-07 17:35:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,9 @@ end
 
 
 def get_japanese_emoticon
-  # code goes here
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
 def get_english_meaning
